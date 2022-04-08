96b72bfb7bb4a3b3594194e2d7b1d54280e3af7f
Navneet Kumar <neet.iitk@gmail.com>
2022-04-08 11:30:46 +1000

    Initial commit

---
+++ src/js/problem.test.js
@@ -0,0 +1,5 @@
+import { sum } from "./problem";
+
+test("sum works", () => {
+  expect(sum(1, 2)).toBe(3);
+});
---
+++ src/ts/problem.test.ts
@@ -0,0 +1,5 @@
+import { sum } from "./problem";
+
+test("sum should works", () => {
+  expect(sum(1, 2)).toBe(3);
+});
